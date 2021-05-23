0ea97fd37f3404f1743c0b2d6a473f6161462064
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-05-23 13:27:26 +0100

    :sparkles: Added excerpts and section heading to home page

--- components/ArticleList/index.js
+++ components/ArticleList/index.js
@@ -3,22 +3,29 @@ import ExternalLinkSvg from "./ExternalLinkSvg";
 
 export default function ArticleList({ articles }) {
   return (
-    <section className={Styles.articleList}>
-      {articles.map((article) => (
-        <a
-          href={article.url}
-          rel="nofollow noopener"
-          className={Styles.articleList__card}
-          key={article.sys.id}
-          target="_blank"
-        >
-          <h3 className={Styles.article__title}>{article.title}</h3>
-          <div className={Styles.article__footer}>
-            <p className={Styles.article__author}>{article.author}</p>
-            <ExternalLinkSvg />
-          </div>
-        </a>
-      ))}
+    <section>
+      <h2 className={Styles.sectionHeading}>
+        Read {articles.length} articles by men on equality in the tech industry
+      </h2>
+      <div className={Styles.articleList}>
+        {articles.map((article) => (
+          <a
+            aria-label={`${article.title} by ${article.author}`}
+            href={article.url}
+            rel="nofollow noopener"
+            className={Styles.articleList__card}
+            key={article.sys.id}
+            target="_blank"
+          >
+            <h3 className={Styles.article__title}>{article.title}</h3>
+            <p className={Styles.article__excerpt}>{article.excerpt}</p>
+            <div className={Styles.article__footer}>
+              <p className={Styles.article__author}>{article.author}</p>
+              <ExternalLinkSvg />
+            </div>
+          </a>
+        ))}
+      </div>
     </section>
   );
 }
