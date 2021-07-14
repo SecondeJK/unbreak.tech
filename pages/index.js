dc8404e22a04c65ca7db3db18dcc0b4a60cd88fa
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-07-14 14:23:07 +0100

    :sparkles: Add favicons

--- pages/index.js
+++ pages/index.js
@@ -16,13 +16,22 @@ export default function Home({ articles }) {
             name="description"
             content="UNBREAK.TECH — a platform for MEN to educate other MEN about the need for change and equality in tech."
           />
-          {/* <link rel="icon" href="/favicon.ico" /> */}
 
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;700&display=swap"
             rel="stylesheet"
           />
+
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="theme-color" content="#000000" />
         </Head>
 
         <LogoBanner />
