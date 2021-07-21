59efc66d1a07b80cf30697bc6ad8dd53956dbe90
Chris Manson <chris@manson.ie>
2021-07-21 14:41:51 +0100

    add github link in the footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,10 +1,12 @@
 import Styles from "@styles/Footer.module.css";
+import GitHub from "@components/Footer/svg/Github";
 
 export default function Footer() {
   const date = new Date();
 
   return (
     <footer className={Styles.footer}>
+      <a href="https://github.com/whitep4nth3r/unbreak.tech" target="_blank" rel="noopener">Contribute on GitHub <div><GitHub /></div></a>
       <p className={Styles.footer__copyright}>&copy; unbreak.tech {date.getFullYear()}</p>
     </footer>
   );
